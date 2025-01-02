5fab7bc02541541032b167d2fc4890525be5cb81
Irene_home <puigparnaui@gmail.com>
2025-01-02 20:04:07 +0100

    ex02 finished

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,16 +6,23 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 16:33:26 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/12/21 16:37:03 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2025/01/02 20:03:31 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <iostream>
 
 int	main(void)
 {
-	std::string	str;
-	
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
 
-	str = "HI THIS IS BRAIN"
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 }
