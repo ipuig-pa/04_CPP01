e01186f7141e4d3a6dc7800456f6b2edb7ee5e7a
Irene_home <puigparnaui@gmail.com>
2025-01-02 19:42:14 +0100

    ex01 finished

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,14 +6,12 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 14:00:37 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/12/21 16:24:10 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2025/01/02 19:40:38 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-
-//do I have to include any checking function to check for proper allocation?
 Zombie* zombieHorde( int N, std::string name )
 {
 	Zombie	*zombie_horde;
